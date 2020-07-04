e15538a965d8d1127ef6f70a26eac7c55e0593a0
VasiliKavaleu <vasiliy.kavaleu@gmail.com>
2020-07-05 00:21:57 +0300

    paginator

--- shop/urls.py
+++ shop/urls.py
@@ -5,8 +5,7 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),
     path('products', views.product_list, name='product_list'),
-    path('<slug:category_slug>/', views.product_list,
-        name='product_list_by_category'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 
     path('delivery', views.delivery, name='delivery'),
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from shop import paginator_helper
 
 
 def index(request):
@@ -15,10 +16,10 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'product/list.html',
-                                {'category': category,
-                                'categories': categories,
-                                'products': products})
+    context = paginator_helper.pg_records(request, products, 9)
+    context[category] = category
+    context[categories] = categories
+    return render(request, 'product/list.html', context)
 
 def product_detail(request, id, slug):
     """Отображение описания товара."""
