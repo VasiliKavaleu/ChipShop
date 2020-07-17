427344ba4cb03f7ba9772a7159efb8c516c0a7e1
VasiliKavaleu <vasiliy.kavaleu@gmail.com>
2020-07-18 00:17:03 +0300

    changed media path

--- ChipShop/urls.py
+++ ChipShop/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
